2bccf7a32b806adb1f1926280eaefc06ff9a6e62
Артем <lol43rus@mail.ru>
2023-06-06 20:53:57 +0300

    Исправил ошибки после созвона

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,14 @@
 let navMain = document.querySelector('.main-nav__wrapper');
 let navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav__wrapper--nojs');
 
 navToggle.addEventListener('click', function () {
-if (navMain.classList.contains('main-nav--closed')) {
-  navMain.classList.remove('main-nav--closed');
-  navMain.classList.add('main-nav--opened');
-} else {
-  navMain.classList.add('main-nav--closed');
-  navMain.classList.remove('main-nav--opened');
-}
-});
+  if (navMain.classList.contains('main-nav__wrapper--closed')) {
+    navMain.classList.remove('main-nav__wrapper--closed');
+    navMain.classList.add('main-nav__wrapper--opened');
+  } else {
+    navMain.classList.add('main-nav__wrapper--closed');
+    navMain.classList.remove('main-nav__wrapper--opened');
+  }
+  });
